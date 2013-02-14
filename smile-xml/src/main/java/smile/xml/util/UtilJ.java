266c06dd6da9ee4e5d8c8b5e78dfea67510b30fb
Stefan Katscher <skatscher@gmail.com>
2013-02-14 23:45:45 +0100

    implement indent option for document.to_s and document.save

--- smile-xml/src/main/java/smile/xml/util/UtilJ.java
+++ smile-xml/src/main/java/smile/xml/util/UtilJ.java
@@ -231,16 +231,17 @@ public class UtilJ {
 		}
 	}
 
-	public static void write( Writer writer, Node node, boolean intent, String encoding ) {
+	public static void write( Writer writer, Node node, boolean indent, String encoding) {
 		try {
 				StreamResult result = new StreamResult(writer);
 				DOMSource source = new DOMSource(node);
 				Transformer transformer = getTransformer();
 				
+				transformer.setOutputProperty(OutputKeys.INDENT, indent ? "yes" : "no");
+				
 				if( encoding != null ) {
 					transformer.setOutputProperty( OutputKeys.ENCODING, encoding );
 				}
-				
 				
 				transformer.transform( source, result);				
 				
@@ -252,21 +253,23 @@ public class UtilJ {
 	}
 
 
-	public static String toString(Node node, boolean escape ) {
-		return toString( node, escape, (String) null );
+	public static String toString(Node node, boolean indent) {
+		return toString(node, indent, (String) null );
 	}
 	
-	public static String toString(Node node, boolean escape, EncodingJ encoding ) {
-		return toString( node, escape, encoding == null ? null : encoding.asJavaString() );
+	public static String toString(Node node, boolean indent, EncodingJ encoding) {
+		return toString(node, indent, encoding == null ? null : encoding.asJavaString() );
 	}
 	
-	public static String toString(Node node, boolean escape, String encoding ) {
+	public static String toString(Node node, boolean indent, String encoding) {
 		StringWriter writer = new StringWriter();
 		try {
 			try { 
 				StreamResult result = new StreamResult(writer);
 				DOMSource source = new DOMSource(node);
 				Transformer transformer = getTransformer();
+				
+				transformer.setOutputProperty(OutputKeys.INDENT, indent ? "yes" : "no");
 				
 				//OMIT_XML_DECLARATION
 				if( encoding != null )
